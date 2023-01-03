ff7896544803e2220f81b707c92835a0e2da1357
shubhams83 <shubhams@techis.io>
2023-01-03 15:21:04 +0530

    added images in components

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,11 @@ import delIcon from "../assets/img/delete-icon.png";
 import userIcon from "../assets/img/user-icon.png";
 import "../assets/css/ContactCard.css";
 
-const ContactCard = ({ contact }) => {
+// const ContactCard = ({ contact })
+const ContactCard = (props) => {
+  // object destructing properties
+  const { id, name, email } = props.contact;
+
   return (
     <>
       <div className="contactCard">
@@ -14,8 +18,10 @@ const ContactCard = ({ contact }) => {
             alt="user-icon"
           />
           <div className="contactCard__userName">
-            <h2>{contact.name}</h2>
-            <p>{contact.email}</p>
+            {/* <h2>{contact.name}</h2> */}
+            <h2>{name}</h2>
+            {/* <p>{contact.email}</p> */}
+            <p>{email}</p>
           </div>
         </div>
         <div className="contactCard__delIcon">
